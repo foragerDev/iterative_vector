ac61325b1d2ffa1022dc5949ed53208e4d6d7b36
foragerDev <mohsan0073@gmail.com>
2021-11-21 01:05:07 +0500

    Add code structur.

---
+++ iterative_vector_const_iterator.hpp
@@ -0,0 +1,13 @@
+#include <iterator>
+
+template<typename ITV>
+class const_it_vector_iterator{
+    using value_type = typename ITV::value_type;
+    using iterator_category = std::random_access_iterator_tag;
+    using pointer = value_type*;
+    using reference = value_type*;
+    using difference_type = std::ptrdiff_t;
+
+
+    
+};
